99326cd6d7465489acb6bf9584f1182e3a648b1b
iillyyaa2033 <iillyyaa2033@mail.ru>
2016-03-29 23:53:35 +0300

    Now I will try to move AndroidEditor on fragments.

---
+++ apps/AndroidEditor/src/com/iillyyaa2033/mud/editor/activity/MainActivityF.java
@@ -0,0 +1,7 @@
+package com.iillyyaa2033.mud.editor.activity;
+
+import android.support.v4.app.FragmentActivity;
+
+public class MainActivityF extends FragmentActivity
+{
+}
---
+++ apps/AndroidEditor/src/com/iillyyaa2033/mud/editor/fragment/MainListFragment.java
@@ -0,0 +1,6 @@
+package com.iillyyaa2033.mud.editor.fragment;
+
+import android.support.v4.app.Fragment;
+
+public class MainListFragment extends Fragment{
+}
