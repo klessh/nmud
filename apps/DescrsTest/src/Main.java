e997c30c7c8bd29f4fd32be7bae5ead93c0dc065
iillyyaa2033 <iillyyaa2033@mail.ru>
2016-06-09 22:31:26 +0300

    DescrsTest update

--- apps/DescrsTest/src/Main.java
+++ apps/DescrsTest/src/Main.java
@@ -12,10 +12,10 @@ public class Main {
 		dict = new Dictionary();
 		
 		objects = new ArrayList<WorldObject>();
-		objects.add(new WorldObject("room", 1, 1, 18, 18, 0));
-		objects.add(new WorldObject("obj", 5, 5, 6, 6, 1, new Material("черный","дерево")));
-		objects.add(new WorldObject("obj", 7, 7, 9, 9, 2, new Material("серая","картон")));
-		objects.add(new WorldObject("obj", 8, 4, 11, 7, 3, new Material("прозрачный","стекло")));
+		objects.add(new WorldObject("room", 1, 1, 18, 18, 101001));
+		objects.add(new WorldObject("obj", 5, 5, 6, 6, 101002, new Material("черный","дерево")));
+		objects.add(new WorldObject("obj", 7, 7, 9, 9, 101003, new Material("серая","картон")));
+		objects.add(new WorldObject("obj", 8, 4, 11, 7, 101004, new Material("прозрачный","стекло")));
 
 		System.out.println(altDescription(10, 10, 0));
 	}
@@ -29,21 +29,62 @@ public class Main {
 		zones = new ArrayList<DescribedZone>();
 		links = new HashMap<Integer,Integer[]>();	// связь между id комнаты и id объектов в ней
 
+		fillDescribed();
+		
+		String result = "\n\n";
+		result += "YOU_ARE_IN ";
+
+		int whereAmI;
+
+		try {
+			whereAmI = getZoneOf(x0, y0);
+			result += toCollocation(getTextualDirection(getPositionCode(x0,y0,zones.get(whereAmI))));
+			result += " OF ";
+			result += zones.get(whereAmI).name;
+		} catch (MudException e) {
+			result += "UNKNOWN_ZONE";
+			return result;
+		}
+		
+		result += ". THERE_ARE ";
+
+		String[] names = new String[links.get(whereAmI).length];
+		int i = 0;
+		for (int j : links.get(whereAmI)) {
+			names[i] = dict.getWord(objs.get(j - 1).name);
+			i++;
+		}
+		result += describeArray(names, true) + ".";
+
+
+		for (int objId : links.get(whereAmI)) {
+			result += "\n\t";
+			result += describeObjectInZone(objId - 1, whereAmI);
+			result += ". ";
+			result += describeObject(objId - 1);
+			result += ". ";
+		}
+
+		return result;
+	}
+
+	// заполняем массивы objs, zones, links
+	static void fillDescribed(){
 		for (int i = 0; i < objects.size(); i++) {
 			WorldObject obj = objects.get(i);
 
 			if (obj.type == null) {
-				log("Object haven't type " + obj.name);
+				log("Object haven't type " + dict.getWord(obj.name));
 			} else if (obj.type.toLowerCase().equals("zone") || obj.type.toLowerCase().equals("room")) {
 				DescribedZone object = new DescribedZone(i, obj);
 				object.area = getDistance(object.x, object.y, object.x, object.y2) * getDistance(object.x, object.y, object.x2, object.y);
 				zones.add(object);
-				log("Added zone " + i + " " + object.name + " " + object.x + ":" + object.y + " " + object.x2 + ":" + object.y2);
+				log("Added zone " + i + " " + dict.getWord(object.name) + " " + object.x + ":" + object.y + " " + object.x2 + ":" + object.y2);
 			} else {
 				DescribedObject object = new DescribedObject(i, obj);
 				object.area = getDistance(object.x, object.y, object.x, object.y2) * getDistance(object.x, object.y, object.x2, object.y);
 				objs.add(object);
-				log("Added object " + i + " " + object.name + " " + object.x + ":" + object.y + " " + object.x2 + ":" + object.y2);
+				log("Added object " + i + " " + dict.getWord(object.name) + " " + object.x + ":" + object.y + " " + object.x2 + ":" + object.y2);
 			}
 		}
 
@@ -63,62 +104,6 @@ public class Main {
 				}
 			}
 		}
-
-		String result = "\n\n";
-		result += "YOU_ARE_IN ";
-
-		int whereAmI;
-
-		try {
-			whereAmI = getZoneOf(x0, y0);
-			result += getTextualKeyKode(getPositionCode(x0,y0,zones.get(whereAmI)));
-			result += " OF ";
-			result += zones.get(whereAmI).name;
-		} catch (MudException e) {
-			result += "UNKNOWN_ZONE";
-			return result;
-		}
-		result += ". THERE_ARE ";
-
-		String[] names = new String[links.get(whereAmI).length];
-		int i = 0;
-		for (int j : links.get(whereAmI)) {
-			names[i] = dict.getNounForm(objs.get(j - 1).name,0);
-			i++;
-		}
-		result += describeArray(names, true) + ".";
-
-
-		for (int objId : links.get(whereAmI)) {
-			result += "\n\t";
-			result += describeObjectInZone(objId - 1, whereAmI);
-			result += ". ";
-			result += describeObject(objId - 1);
-			result += ". ";
-		}
-
-		return result;
-	}
-
-	static String toNativeLang(int noun, int verb){
-		return "";
-	}
-	
-	static String toNativeLang(Integer noun, Integer[] nounAttrs, Integer verb, Integer[] verbAttrs){
-		int nounType = noun/10;	// часть речи подлежащего
-		int nounGen; // род подлежащего
-		int nounCou; // число подлежащего
-		String formedNoun;	// просклоненное подлежащее
-		
-		if(nounType == 0){
-			nounCou = (int) noun/100;
-			formedNoun = dict.getNounForm((int)noun/1000, 0);
-		} else{
-			nounCou = (int) noun/100;
-			formedNoun = dict.getNounForm((int)noun/1000, 0);
-		}
-		
-		return formedNoun;
 	}
 	
 	// перебираем все описанные зоны, чтобы получить ту, в которой сейчас игрок
@@ -136,6 +121,43 @@ public class Main {
 		return id;
 	}
 
+	// возвращает массив со словами, опред местоположение в зоне
+	static int[] getTextualDirection(int code){
+		// TODO: тут неправильно. Шурик, это не наш метод!
+		// Как правильно - пока не знаю.
+		switch (code) {
+			case 1010:
+//				result += "DOWN_LEFT_CORNER";
+				return new int[]{102001,102002,101025};
+			case 1020:
+//				result += "DOWN_WALL";
+				return new int[]{102001,101026};
+			case 1030:
+//				result += "DOWN_RIGHT_CORNER";
+				return new int[]{102001,102003,101025};
+			case 2010:
+//				result += "LEFT_WALL";
+				return new int[]{102002,101026};
+			case 2020:
+//				result += "CENTER";
+				return new int[]{101027};
+			case 2030:
+//				result += "RIGHT_WALL";
+				return new int[]{102003,101026};
+			case 3010:
+//				result += "TOP_LEFT_CORNER";
+				return new int[]{102004,102002,101025};
+			case 3020:
+//				result += "TOP_WALL";
+				return new int[]{102004,101026};
+			case 3030:
+//				result += "TOP_RIGHT_CORNER";
+				return new int[]{102004,102003,101025};
+			default:
+				return new int[]{0};
+		}
+	}
+	
 	static String describeArray(Object[] list, boolean toLowerCase) {
 		StringBuilder sb = new StringBuilder();
 
@@ -184,7 +206,7 @@ public class Main {
 		String result = "";
 		result += object.name;
 		result += " LOCATED_NEAR ";
-		result += getTextualKeyKode(getPositionCode(object, zone));
+		result += getTextualDirection(getPositionCode(object, zone));
 
 		return result;
 	}
@@ -219,64 +241,29 @@ public class Main {
 		return result;
 	}
 
-	static String getTextualKeyKode(int code){
-		String result = "";
-		// TODO: тут неправильно. Шурик, это не наш метод!
-		// Как правильно - пока не знаю.
-		switch (code) {
-			case 1010:
-				result += "DOWN_LEFT_CORNER";
-				break;
-			case 1020:
-				result += "DOWN_WALL";
-				break;
-			case 1030:
-				result += "DOWN_RIGHT_CORNER";
-				break;
-			case 2010:
-				result += "LEFT_WALL";
-				break;
-			case 2020:
-				result += "CENTER";
-				break;
-			case 2030:
-				result += "RIGHT_WALL";
-				break;
-			case 3010:
-				result += "TOP_LEFT_CORNER";
-				break;
-			case 3020:
-				result += "TOP_WALL";
-				break;
-			case 3030:
-				result += "TOP_RIGHT_CORNER";
-				break;
-			default:
-				result += "ERROR";
-		}
-		return result;
+
+	static String toNativeLang(int noun, int verb){
+		return "";
 	}
 	
-	static StringBuilder recursiveDescr(StringBuilder sb, HashMap<Integer,DescribedObject> objsm, int[] pie, int deep, DescribedObject starter) {
-		if (deep > 10) return sb;
-		if (!starter.included) return sb;
-		deep++;
-
-		DescribedObject left2 = objsm.get(starter.obj_left2);
-		DescribedObject left = objsm.get(starter.obj_left);
-		DescribedObject right = objsm.get(starter.obj_right);
-		DescribedObject right2 = objsm.get(starter.obj_right2);
-
-	//	sb.append(left.included ? "Слева от " + starter.name.toLowerCase() + "а находится " + left.name.toLowerCase() + "; " : "");
-	//	sb.append(right.included ? "Справа от " + starter.name.toLowerCase() + "а находится " + right.name.toLowerCase() + "; " : "");
-		starter.included = false;
-
-		if (left.included) recursiveDescr(sb, objsm, pie, deep, left);
-		if (right.included) recursiveDescr(sb, objsm, pie, deep, right);
-
-		return sb;
+	// строит фразу из id слов
+	static String toNativeLang(Integer noun, Integer[] nounAttrs, Integer verb, Integer[] verbAttrs){
+		String formedNoun = "";	// просклоненное подлежащее
+		
+		return formedNoun;
+	}
+	
+	static String toCollocation(int... ids){
+		StringBuilder sb = new StringBuilder();
+		for(int i : ids){
+			sb.append(dict.getWord(i)).append(" ");
+		}
+		return sb.toString();
 	}
 
+	//
+	//	ГЕОМЕТРИЧЕСКИЕ МЕТОДЫ
+	//
 	static private int getDistance(int x1, int y1, int x2, int y2) {
 		// расстояние между точками x и у.
 		double dist = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
@@ -299,64 +286,10 @@ public class Main {
 		return new Double(Math.toDegrees(arccos)).intValue();
 	}
 
-	String compareObjs(int angle, DescribedObject obj, int[] pie) {
-		StringBuilder sb = new StringBuilder();
 
-		int mark = 0;
-
-		int fr = ((angle + 45) < 360 ? angle + 45 : (360 - (angle + 45)) * -1);
-		int br = ((fr + 90) < 360 ? fr + 90 : (360 - (fr + 90)) * -1);
-		int bl = ((br + 90) < 360 ? br + 90 : (360 - (br + 90)) * -1);
-		int fl = ((bl + 90) < 360 ? bl + 90 : (360 - (bl + 90)) * -1);
-
-		if (waka(fl, fr, obj)) mark = 1;
-		else if (waka(fr, br, obj)) mark = 2;
-		else if (waka(br, bl, obj)) mark = 3;
-		else if (waka(bl, fl, obj)) mark = 4;
-		else mark = 5;
-
-		String[] straight = {"Перед вами","Прямо перед вами"};
-		String[] right = {"Справа","Справа от вас","По правую руку"};
-		String[] left = {"Слева","Слева от вас"};
-		String[] back = {"Позади","Позади вас"};
-		String[] verb = {"виднеется","находится"};
-
-		switch (mark) {
-			case 1:
-				sb.append(straight[(int)(Math.random() * (straight.length))]);
-				//	sb.append("Впереди ");
-				break;
-			case 2:
-				sb.append(right[(int)(Math.random() * (right.length))]);
-				break;
-			case 3:
-				sb.append(back[(int)(Math.random() * back.length)]);
-				break;
-			case 4:
-				sb.append(left[(int)(Math.random() * left.length)]);
-				break;
-			case 5:
-			default:
-				send(0, "Пофэйлили марку.");
-		}
-	//	sb.append(" ").append(verb[(int) (Math.random() * verb.length)]).append(" ").append(obj.name.toLowerCase());
-
-		return sb.toString();
-	}
-
-	boolean waka(int l, int r, DescribedObject obj) {
-
-		if (l < r) {
-			if (obj.deg_center >= l && obj.deg_center <= r)
-				return true;
-		} else {
-			if (obj.deg_center >= l) return true;
-			if (obj.deg_center <= r) return true;
-		}
-
-		return false;
-	}
-
+	//
+	//	МЕТОДЫ ВВОДА-ВВЫВОДА
+	//
 	void send(int i, String s) {
 		System.out.println(s);
 	}
@@ -367,4 +300,3 @@ public class Main {
 }
 
 class MudException extends Exception {}
-
