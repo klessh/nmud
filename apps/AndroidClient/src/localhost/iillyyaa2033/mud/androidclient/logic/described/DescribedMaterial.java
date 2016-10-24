81fa708f4a6a0daa7bbd1942b4e0356e600892b8
iillyyaa2033 <iillyyaa2033@mail.ru>
2016-10-24 23:40:29 +0300

    Deleted unused stuff

--- apps/AndroidClient/src/localhost/iillyyaa2033/mud/androidclient/logic/described/DescribedMaterial.java
+++
@@ -1,9 +0,0 @@
-package localhost.iillyyaa2033.mud.androidclient.logic.described;
-
-public class DescribedMaterial{
-	// Это - вспом. класс для генератора описаний. В нем должны содержаться слова и/или
-	// фразы (объекты классов Word и Graph соотв.)!
-	
-	// TODO: кач-ва строками, падежи, роды
-	// TODO: рассчет наложения цветов
-}
